157ec2926b29add9ba67cdf4b9187d02844fe2c3
KshitizAgarwal21 <kshitizagarwal27@gmail.com>
2021-07-05 23:41:26 +0530

    Corrected before install shell

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,18 +1,15 @@
-version: 0.0
-os: linux
-files:
-  - source: /
-    destination: /home/ec2-user/express-app
-hooks:
-  ApplicationStop:
-    - location: scripts/application_stop.sh
-      timeout: 300
-      runas: ec2-user
-  BeforeInstall:
-    - location: scripts/before_install.sh
-      timeout: 300
-      runas: ec2-user
-  ApplicationStart:
-    - location: scripts/application_start.sh
-      timeout: 300
-      runas: ec2-user
+#!/bin/bash
+
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/express-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
